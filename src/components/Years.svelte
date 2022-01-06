82a777dcd4b4c87e18f09a00aa3c17b37bd3b43e
6-000-000 <bobo_ross@delsur.io>
2022-01-06 11:41:18 +0800

    Render number of years based on current year if no end is specified

---
+++ src/components/Years.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { DateTime } from 'luxon'
+
+  export let start: number
+  export let end: number = null
+
+  function years(start: number, end?: number): number {
+    const endYear = end || Number.parseInt(DateTime.now().toFormat('y'), 10)
+    const difference = endYear - start
+    return difference
+  }
+</script>
+
+<div class="text-3xl font-bold">{years(start, end)} years</div>
